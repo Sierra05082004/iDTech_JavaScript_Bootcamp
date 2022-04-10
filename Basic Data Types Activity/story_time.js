41ce7251c738f450023688a6d41b402252c9d298
Emiliano Sierra Hernández <emisieher@lancaster.edu.mx>
2022-04-10 17:21:16 -0500

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,37 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let nameOf = 'Booga';
+let statusOf = " wife, 2 sons, a dauther and a pet tiger,";
+let place = 'Ameriland';
+let weapon = "giant wood stick."
+let proudOf = " to collect bones."
+// TODO: Create 3-Integer variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let animalsPerDay = 3;
+let hoursOfSleep = 2;
+let boneCollection = 400;
+let year = 40000;
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let animalsToHunt = ['mammoths','tigers','giant armadillos', 'turtles'];
+let hobbies = ["smashing rocks", "swiming with sharks","playing with our baby tiger"]
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let considerIntelligent = true;
+let dangersProblem = true;
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("Hello children, my name is " + nameOf +'.');
+console.log("I'm going to tell you a bit about my story in " + year + "b.C.");
+console.log("I lived in a place called " + place+'.');
+console.log("My family consisted of my" + statusOf + " we lived very happily.");
+console.log("For us to survive we needed to hunt " + animalsPerDay + " animals per day.");
+console.log("Some of this animals were " + animalsToHunt[0] + " or "+ animalsToHunt[2]+'.');
+console.log("I used to hunt them with my " + weapon);
+console.log("Because of all of the dangers arround us, we only had " + hoursOfSleep + " hours of sleep per day." + "\n");
+console.log("Its "+ dangersProblem + " that dangers were a problem, but that didn't stopped me from having fun.")
+console.log("I had a lot of hobbies like " + hobbies[0]+ ", " +hobbies[1]+" or "+ hobbies[2]+".")
+console.log("My favourite thing to do was"+proudOf+ " I had about "+boneCollection+" in my collection.\n")
+console.log("Some of you asked me if I was intelligent.")
+console.log("it's " +considerIntelligent+ " I was very intelligent for my time.\n" )
+console.log("That was my story kids, feel free to ask any questions.")
